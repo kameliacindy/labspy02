318080713f3904262b270faa26d54f001f9320c8
kameliacindy <kamelcindy60@gmail.com>
2019-11-02 23:13:07 +0700

    Upload

---
+++ statementif.py
@@ -0,0 +1,39 @@
+print("Mencari nilai terbesar dari 3 bilangan..."
+      "\nKetiga bilangan harus bernilai integer!"
+      "\nSilahkan masukan bilangan...")
+
+Bil1 = int(input("Masukan Bilangan 1 = "))
+Bil2 = int(input("Masukan Bilangan 2 = "))
+Bil3 = int(input("Masukan Bilangan 3 = "))
+print("\n")
+
+# Mencari 1 Bilangan terbesar
+if (Bil1 > Bil2) and (Bil1 > Bil3):
+    Terbesar = Bil1
+    Nam_Bil = "Bilangan 1"
+elif (Bil2 > Bil1) and (Bil2 > Bil3):
+    Terbesar = Bil2
+    Nam_Bil = "Bilangan 2"
+elif (Bil3 > Bil1) and (Bil3 > Bil2):
+    Terbesar = Bil3
+    Nam_Bil = "Bilangan 3"
+
+# Mencari 2 Bilangan terbesar
+elif (Bil1 == Bil2) and (Bil1 != Bil3):
+    Terbesar = Bil1
+    Nam_Bil = "Bilangan 1 dan Bilangan 2"
+elif (Bil1 == Bil3) and (Bil1 != Bil2):
+    Terbesar = Bil3
+    Nam_Bil = "Bilangan 1 dan Bilangan 3"
+elif (Bil2 == Bil3) and (Bil2 != Bil1):
+    Terbesar = Bil2
+    Nam_Bil = "Bilangan 2 dan Bilangan 3"
+
+# Mencari semua bilangan sama besar
+elif [Bil1, Bil2, Bil3] == [Bil1, Bil2, Bil3]:
+    Terbesar = Bil1
+    Nam_Bil = "semua bilangan sama besar"
+else:
+    print("Nilai yang anda masukan salah!")
+
+print("Bilangan yang terbesar adalah", Nam_Bil, "dengan nilai", Terbesar)
